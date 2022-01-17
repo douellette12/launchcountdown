14ef0137c7879fe1528a90a3894fa2ad5853b46d
douellette12 <39159681+douellette12@users.noreply.github.com>
2022-01-16 20:15:34 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,14 @@
-var launchDate = new Date(2021, 11, 25)
+var launchDate = new Date(2023, 9, 14)
 
 var x = setInterval(() => {
     var now = new Date()
     
     var timeFrom =   launchDate.getTime() - now.getTime()
-    var years = launchDate.getFullYear() - now.getFullYear()
     var days = Math.floor(timeFrom / (1000 * 60 * 60 * 24))
     var hours = Math.floor(timeFrom % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     var minutes = Math.floor(timeFrom % (1000 * 60 * 60) / (1000 * 60))
     var seconds = Math.floor(timeFrom % (1000 * 60) / 1000)
 
-    document.getElementById("years").innerHTML = years
     document.getElementById("days").innerHTML = days
     document.getElementById("hours").innerHTML = hours
     document.getElementById("minutes").innerHTML = minutes
